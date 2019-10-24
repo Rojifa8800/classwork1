226b06c9eacda17de9ec3900b93d1c30c724c9e2
Rojifa8800 <56949418+Rojifa8800@users.noreply.github.com>
2019-10-24 10:49:44 +0600

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,18 @@
+a, b, c = 5, 3.2, "\"Hello\""
+
+print(a)
+print(b)
+print(c)
+
+print("%s %s %s" (a,b,c))
+print("%s\n%s\n%s" %(a,b,c))
+
+print('I love {} and {}'.format('bread','butter'))
+print('I love {0} and {1}'.format('bread','butter'))
+
+
+age=23
+message= "Happy"+ str (age)+"rd Birthday!"
+Y=10
+X=
+print=("Happy {}th Birthday!".format (y))
