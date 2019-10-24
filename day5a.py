9af17ace8aeb1cd1c09382224bbf57bc89554895
Rojifa8800 <56949418+Rojifa8800@users.noreply.github.com>
2019-10-24 11:42:23 +0600

    Add files via upload

---
+++ day5a.py
@@ -0,0 +1,14 @@
+#Python for Loop
+#===================================
+
+"""
+Synatax offor Loop
+
+for variable in sequence:
+  Body of for
+"""
+for x in [7,33,5,22]:
+   print(x,end="\t")
+
+for x in range(0,99):
+   print(x)
